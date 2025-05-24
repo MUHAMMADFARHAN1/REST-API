65a45bb6cf526fd19e70314f092e1aacfff9b612
Muhammad Farhan <farhan_75230@hotmail.com>
2025-05-24 10:21:03 +0200

    Database SImulation Added!

--- server.js
+++ server.js
@@ -1,8 +1,38 @@
 import express from "express";
 
+///////////////////////Server Basic Info/////////////////////////////
 const PORT = 5001;
 const app = express();
 
 app.listen(PORT, () => {
   console.log("server is running...");
 });
+
+//////////////////////Database Simulation////////////////////////////
+// Simulation of database
+let Product = [
+  {
+    name: "First",
+    description: "Product 1",
+    price: "10",
+    quantity: "5",
+    createdAt: "01/12/2022",
+  },
+  {
+    name: "Second",
+    description: "Product 2",
+    price: "20",
+    quantity: "10",
+    createdAt: "11/12/2022",
+  },
+  {
+    name: "Third",
+    description: "Product 3",
+    price: "30",
+    quantity: "15",
+    createdAt: "21/12/2022",
+  },
+];
+
+// Order will be empty initially as thez will be dispatched later on
+let Order = [];
